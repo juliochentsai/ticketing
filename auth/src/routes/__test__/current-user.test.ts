5c2f64d420ffd2c7f1ffc3ef034746ae5a6558d3
Julio Chen <julio111777@hotmail.com>
2025-04-01 09:08:47 -0400

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -7,7 +7,7 @@ it("responds with details baout the current user", async () => {
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
     .send()
-    .expect(400); //Must be 200
+    .expect(200); //Must be 200
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
